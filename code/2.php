7ac0a4081e167092cdaa9b742b9e9cf4e0c54dfe
krisstish <aks.kristina1@gmail.com>
2022-02-26 18:59:26 +0200

    number2

---
+++ code/2.php
@@ -0,0 +1,16 @@
+<?php
+$a = 1;
+echo $a;
+
+$b = 2;
+echo "\n<br>"; 
+echo $b;
+
+$c = 3.3;
+echo "\n<br>",$c;
+
+echo "\n<br>",7+5;
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo "\n<br>", $last_month - $this_month;
